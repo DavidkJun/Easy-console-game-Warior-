85e0817f58e1fd21aadeb561a20bd6ff8cc38e28
Davyd Krykota <144104449+DavidkJun@users.noreply.github.com>
2024-04-06 20:24:36 +0300

    Update game.js
    Added class worrior for interacting with character

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
-
+class Warrior{
+  constructor(){
+   this.lvl = 1 ;
+   this.warrior_rank = "Pushover";
+    this.warrior_ranks = ["Pushover", "Novice", "Fighter", "Warrior", "Veteran", "Sage", "Elite", "Conqueror", "Champion", "Master", "Greatest"];
+    this.warrior_experiance = 100;
+    this.warrior_achievements =[];
+      }
